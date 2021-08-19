a2649e859e3aeeb5c0a462ee777526650c89a77c
Ronaldo Augusto de Morais Sobrinho <ronaldoaugustodemorais@gmail.com>
2021-08-19 23:57:42 +0000

    fim da aula 2

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, request, session, g, redirect, abort, render_template, flash
+
+#configuracao
+DATABASE = "blog.db"
+SECRET_KEY = 'pudim'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def antes_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def depois_request(exc):
+    g.bd.close()
+
+@app.route('/')
+def exibir_entradas():
+    return render_template('exibir_entradas.html')
 
 @app.route('/hello')
 def pagina_inicial():
